970b87b3127b4596ccf806276778ed1e8de2c37f
vicny135 <vicny135@tlvm-4-4-4.ad.liu.se>
2023-11-22 11:38:39 +0100

    TDDD86 Lab7 redovisning

---
+++ Lab7/patternrecognition/src/fast.cpp
@@ -0,0 +1,93 @@
+#include <QApplication>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <chrono>
+#include "Point.h"
+#include <unordered_map>
+
+// constants
+
+static const int SCENE_WIDTH = 512;
+static const int SCENE_HEIGHT = 512;
+
+void render_points(QGraphicsScene* scene, const vector<Point>& points) {
+    for(const auto& point : points) {
+        point.draw(scene);
+    }
+}
+
+void render_line(QGraphicsScene* scene, const Point& p1, const Point& p2) {
+    p1.lineTo(scene, p2);
+}
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+
+    // open file
+    string filename = "input100.txt";
+    ifstream input;
+    input.open(filename);
+
+    // the vector of points
+    vector<Point> points;
+
+    // read points from file
+    int N;
+    int x;
+    int y;
+
+    input >> N;
+
+    for (int i = 0; i < N; ++i) {
+        input >> x >> y;
+        points.push_back(Point(x, y));
+    }
+    input.close();
+
+    // setup graphical window
+    QGraphicsView *view = new QGraphicsView();
+    QGraphicsScene *scene = new QGraphicsScene(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
+    view->setScene(scene);
+    // draw points to screen all at once
+    render_points(scene, points);
+    view->scale(1, -1); //screen y-axis is inverted
+    view->resize(view->sizeHint());
+    view->setWindowTitle("Fast Sorting Pattern Recognition");
+    view->show();
+
+    // sort points by natural order
+    // makes finding endpoints of line segments easy
+    sort(points.begin(), points.end());
+    auto begin = chrono::high_resolution_clock::now();
+
+    int size = points.size() - 1;
+    for(int p = 0; p < size; p++){
+       // key for slopes, value for vector containing points for the same slope
+       unordered_map<double, vector<Point>> slopes;
+
+       for(int nextPoint = p+1; nextPoint < size + 1; nextPoint++){
+           // add point to vector if it is the same slope as an already existing key
+           // in the map, otherwise add new key and value pair to the unordered map
+           double slope = points[p].slopeTo(points[nextPoint]);
+           slopes[slope].push_back(points[nextPoint]);
+
+           // if the vector for the slope has reached atleast three points,
+           // then render the line
+           if(slopes[slope].size() >= 3){
+               render_line(scene, points[p], points[nextPoint]);
+               a.processEvents();
+           }
+       }
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    cout << "Computing line segments took "
+         << std::chrono::duration_cast<chrono::milliseconds>(end - begin).count()
+         << " milliseconds." << endl;
+    return a.exec(); // start Qt event loop
+}
+
